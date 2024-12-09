606088cfb438d87462ca8bdd011623eaa55ae174
cakfan <taufan.fhm8@gmail.com>
2024-12-09 14:08:51 +0700

    fix: redirect to dashboard

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,11 +1,17 @@
 import { type Metadata } from "next";
 import SignInForm from "./form";
+import { getMe } from "@/actions/user";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
 export default async function Page() {
+  const me = await getMe();
+
+  if (me) redirect("/admin");
+
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
       <SignInForm />
